7b82517030d5552d0bd115cec3307775c1c0d30e
Aizhu Xue <aizhudeveloper@gmail.com>
2024-03-09 06:07:36 -0500

    save Search.js

--- src/features/Search.js
+++ src/features/Search.js
@@ -1,14 +1,13 @@
-import { View } from 'react-native'
 import SearchBar from '../components/SearchBar'
 import RestaurantList from '../components/RestaurantList'
 
 const Search = () => {
     return (
-        <View>
+        <>
             <SearchBar />
             <RestaurantList />
-        </View>
+        </>
     )
 }
 
-export default Search
+export default Search   
