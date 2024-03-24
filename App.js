a32e08d57e0e5b74f6e3acd7a04833ef32a2d4e6
Aizhu Xue <aizhudeveloper@gmail.com>
2024-03-24 04:55:40 -0400

    put SafeArea into restaurant screen from app.js

--- App.js
+++ App.js
@@ -1,19 +1,9 @@
 import React from "react";
-import { StatusBar } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
-import { styled } from "styled-components/native";
 import { RestaurantScreen } from './src/features/restaurants/screens/restaurants.screen'
-
-const SafeArea = styled(SafeAreaView)`
-  flex: 1;
-  margin-top: ${StatusBar.currentHeight}px;
-`
 
 export default function App() {
   return (
-    <SafeArea>
-      <RestaurantScreen />
-    </SafeArea>
+    <RestaurantScreen />
   );
 }
 
--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,6 +1,8 @@
 import React from 'react'
-import { View, StyleSheet } from 'react-native'
+import { View } from 'react-native'
 import { styled } from 'styled-components/native'
+import { StatusBar } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 import MySearchBar from '../../../components/MySearchBar'
 import RestaurantList from '../components/restaurant-list.component'
 import { sizes } from '../../../utils/Sizes'
@@ -10,12 +12,19 @@ const Search = styled(View)`
     flex: 1;
 `
 
+const SafeArea = styled(SafeAreaView)`
+  flex: 1;
+  margin-top: ${StatusBar.currentHeight}px;
+`
+
 export const RestaurantScreen = () => {
     return (
-        <Search>
-            <MySearchBar />
-            <RestaurantList />
-        </Search>
+        <SafeArea>
+            <Search>
+                <MySearchBar />
+                <RestaurantList />
+            </Search>
+        </SafeArea>
     )
 }
 
