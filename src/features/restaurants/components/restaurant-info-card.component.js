657641c1ffc1fba7d8a62f9d5d605ab615743c5d
Aizhu Xue <aizhudeveloper@gmail.com>
2024-03-25 19:26:09 -0400

    loop and render star icons, fix ui

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { Text, View } from 'react-native'
 import { Card } from 'react-native-paper'
 import styled from 'styled-components/native'
 import { useFonts as useOswald, Oswald_400Regular } from '@expo-google-fonts/oswald'
@@ -14,17 +14,28 @@ const RestaurantCard = styled(Card)`
 `
 const RestaurantCardCover = styled(Card.Cover)``
 
+const RestaurantInfo = styled(View)`
+    flex-direction: row
+`
+const RestaurantDescription = styled(View)`
+`
+
 const Title = styled(Text)`
     font-family: ${props => props.theme.fonts.heading};
     font-size: ${props => props.theme.fontSizes.title};
     font-weight: ${props => props.theme.fontWeights.bold};
     color: ${props => props.theme.colors.ui.error};
-    margin: 10px 0px 5px 10px;
+    margin: 10px 0 0 10px;
 `
 const Address = styled(Text)`
     font-family: ${props => props.theme.fonts.body};
     font-size: ${props => props.theme.fontSizes.body};
     margin: ${props => props.theme.space[2]};
+`
+
+const Ratings = styled(View)`
+    flex-direction: row;
+    
 `
 
 const Star = styled(SvgXml)`
@@ -33,16 +44,6 @@ const Star = styled(SvgXml)`
 `
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
-    const {
-        name = "Some restaurant",
-        icon,
-        photos = ["https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg"],
-        address = "100 Some Random Street",
-        isOpenNow = true,
-        rating = 4,
-        isClosedTemporarily,
-    } = restaurant
-
     const [oswaldLoaded, oswaldError] = useOswald({
         Oswald_400Regular
     })
@@ -54,12 +55,30 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
         return null;
     }
 
+    const {
+        name = "Some restaurant",
+        icon,
+        photos = ["https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg"],
+        address = "100 Some Random Street",
+        isOpenNow = true,
+        rating = 4,
+        isClosedTemporarily,
+    } = restaurant
+
+    const starRatings = [...Array(Math.ceil(rating))].map((_, i) => i)
+
     return (
         <RestaurantCard elevation={5}>
             <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
-            <Title>{name}</Title>
-            <Address>{address}</Address>
-            <Star xml={star} width={20} height={20} />
+            <RestaurantInfo>
+                <RestaurantDescription>
+                    <Title>{name}</Title>
+                    <Address>{address}</Address>
+                </RestaurantDescription>
+                <Ratings>
+                    {starRatings.map(_ => <Star xml={star} width={20} height={20} />)}
+                </Ratings>
+            </RestaurantInfo>
         </RestaurantCard>
     )
 }
