2d99154fe3017d4918c14ac3366103193738dc64
Aizhu Xue <aizhudeveloper@gmail.com>
2024-03-30 08:57:18 -0400

    set up services architecture

--- App.js
+++ App.js
@@ -4,7 +4,6 @@ import { ThemeProvider } from "styled-components";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Ionicons } from '@expo/vector-icons';
-import { styled } from "styled-components/native";
 import { theme } from './src/infrastructure/theme'
 import RestaurantScreen from './src/features/restaurants/screens/restaurants.screen'
 import MapScreen from "./src/features/restaurants/screens/map.screen";
@@ -12,42 +11,46 @@ import SettingScreen from "./src/features/restaurants/screens/settings.screen";
 
 const Tab = createBottomTabNavigator()
 
+const tabIcon = {
+  Restaurants: 'storefront-outline',
+  Map: 'map-outline',
+  Setting: 'settings-outline'
+}
+
+const createScreenOptions = ({ route }) => {
+  const iconName = tabIcon[route.name]
+
+  return ({
+    tabBarIcon: ({ color, size }) => (
+      <Ionicons name={iconName} color={color} size={size} />
+    ),
+    tabBarShowLabel: false,
+    headerShown: false,
+    tabBarActiveTintColor: 'rgba(233, 30, 99, 1)',
+    tabBarStyle: { paddingBottom: 4 }
+  })
+}
+
 export default function App() {
   return (
     <SafeAreaProvider>
       <ThemeProvider theme={theme}>
         <NavigationContainer>
           <Tab.Navigator
-            screenOptions={{
-              tabBarShowLabel: false,
-              headerShown: false,
-              tabBarActiveTintColor: 'rgba(233, 30, 99, 1)',
-              tabBarStyle: { paddingBottom: 4 }
-            }}
+            screenOptions={createScreenOptions}
           >
             <Tab.Screen
-              name="Restaurants" component={RestaurantScreen}
-              options={{
-                tabBarLabel: 'Restaurants',
-                tabBarIcon: ({ color, size }) => (
-                  <Ionicons name="storefront-outline" color={color} size={size} />
-                ),
-              }}
+              name="Restaurants"
+              component={RestaurantScreen}
             />
-            <Tab.Screen name="Map" component={MapScreen}
-              options={{
-                tabBarLabel: 'Map',
-                tabBarIcon: ({ color, size }) => (
-                  <Ionicons name="map-outline" color={color} size={size} />
-                ),
-              }} />
-            <Tab.Screen name="Setting" component={SettingScreen}
-              options={{
-                tabBarLabel: 'Settings',
-                tabBarIcon: ({ color, size }) => (
-                  <Ionicons name="settings-outline" color={color} size={size} />
-                ),
-              }} />
+            <Tab.Screen
+              name="Map"
+              component={MapScreen}
+            />
+            <Tab.Screen
+              name="Setting"
+              component={SettingScreen}
+            />
           </Tab.Navigator>
         </NavigationContainer>
       </ThemeProvider>
