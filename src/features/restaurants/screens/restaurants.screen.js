52c014261a50661a73849f72e3da36ec254e77fe
Aizhu Xue <aizhudeveloper@gmail.com>
2024-04-04 13:30:28 -0400

    add loading indicator to restaurant screen

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
-import { FlatList, Text, View } from "react-native";
-import { Searchbar } from "react-native-paper";
+import { FlatList, View } from "react-native";
+import { Searchbar, ActivityIndicator, MD2Colors } from "react-native-paper";
 import styled from "styled-components/native";
 
 import { SafeArea } from "../../../components/utility/safe-area.component";
@@ -20,6 +20,19 @@ const RestaurantList = React.memo(styled(FlatList).attrs({
     overScrollMode: 'never',
 })``, (prevProps, nextProps) => prevProps.data === nextProps.data)
 
+const LoadingContainer = styled(View)`
+    flex: 1;
+    justify-content: center;
+    align-items: center
+`
+
+const Loading = () => {
+    return (
+        <LoadingContainer>
+            <ActivityIndicator animating={true} color={MD2Colors.red800} size={'large'} />
+        </LoadingContainer>
+    )
+}
 
 const renderItem = ({ item }) => (
     <Spacer position="bottom" size="m">
@@ -34,11 +47,12 @@ export const RestaurantsScreen = () => {
             <SearchContainer>
                 <Searchbar />
             </SearchContainer>
-            <RestaurantList
+            {isLoading ? <Loading /> : <RestaurantList
                 data={restaurants}
                 renderItem={renderItem}
                 keyExtractor={(item) => item.name}
-            />
+            />}
+
         </SafeArea>
     );
 };
