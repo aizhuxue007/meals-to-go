bfa1d3e7bfda93ad514646a14b0751d3dd46c98f
Aizhu Xue <aizhudeveloper@gmail.com>
2024-04-04 15:49:21 -0400

    better star icon key in map()

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,15 +1,15 @@
 import React from "react";
 import {
-  RestaurantCard,
-  RestaurantCardCover,
-  RestaurantInfo,
-  RestaurantDescription,
-  Icons,
-  Ratings,
-  Icon,
-  Status,
-  Open,
-  Star,
+    RestaurantCard,
+    RestaurantCardCover,
+    RestaurantInfo,
+    RestaurantDescription,
+    Icons,
+    Ratings,
+    Icon,
+    Status,
+    Open,
+    Star,
 } from "./restaurant-info-card.styles";
 import { Text } from "../../../components/typography/text.component";
 import star from "../../../../assets/star";
@@ -18,56 +18,57 @@ import open from "../../../../assets/open";
 import Spacer from "../../../components/spacer/spacer.component";
 
 export const RestaurantInfoCard = React.memo(({ restaurant }) => {
-  const {
-    name,
-    icon,
-    photos,
-    vicinity,
-    isOpenNow,
-    rating,
-    isClosedTemporarily,
-  } = restaurant;
+    const {
+        name,
+        icon,
+        photos,
+        vicinity,
+        isOpenNow,
+        rating,
+        isClosedTemporarily,
+        placeId
+    } = restaurant;
 
-  const starRatings = [...Array(5)].map((_, i) => i);
+    const starRatings = [...Array(5)].map((_, i) => i);
 
-  return (
-    <RestaurantCard elevation={5}>
-      <RestaurantCardCover key={name} source={{ uri: photos && photos[0] }} />
-      <RestaurantInfo>
-        <RestaurantDescription>
-          <Spacer position={"bottom"} size={"s"}>
-            <Text variant={"bold"}>{name}</Text>
-          </Spacer>
-          <Spacer position={"bottom"} size={"m"}>
-            <Text variant={"body"}>{vicinity}</Text>
-          </Spacer>
+    return (
+        <RestaurantCard elevation={5}>
+            <RestaurantCardCover key={name} source={{ uri: photos && photos[0] }} />
+            <RestaurantInfo>
+                <RestaurantDescription>
+                    <Spacer position={"bottom"} size={"s"}>
+                        <Text variant={"bold"}>{name}</Text>
+                    </Spacer>
+                    <Spacer position={"bottom"} size={"m"}>
+                        <Text variant={"body"}>{vicinity}</Text>
+                    </Spacer>
 
-          {isClosedTemporarily && (
-            <Text variant={"error"}>Temporarily Closed</Text>
-          )}
-        </RestaurantDescription>
-        <Icons>
-          <Ratings>
-            {starRatings.map((i) =>
-              i < Math.ceil(rating) ? (
-                <Star key={i} xml={star} width={20} height={20} />
-              ) : (
-                <Star key={i} xml={greyStar} width={20} height={20} />
-              ),
-            )}
-          </Ratings>
-          <Status>
-            {isOpenNow && (
-              <Spacer position={"top"} size={"s"}>
-                <Open xml={open} width={16} height={16} />
-              </Spacer>
-            )}
-            <Icon source={{ uri: icon }} />
-          </Status>
-        </Icons>
-      </RestaurantInfo>
-    </RestaurantCard>
-  );
+                    {isClosedTemporarily && (
+                        <Text variant={"error"}>Temporarily Closed</Text>
+                    )}
+                </RestaurantDescription>
+                <Icons>
+                    <Ratings>
+                        {starRatings.map((i) =>
+                            i < Math.ceil(rating) ? (
+                                <Star key={`${placeId}-${i}-yellow`} xml={star} width={20} height={20} />
+                            ) : (
+                                <Star key={`${placeId}-${i}-grey`} xml={greyStar} width={20} height={20} />
+                            ),
+                        )}
+                    </Ratings>
+                    <Status>
+                        {isOpenNow && (
+                            <Spacer position={"top"} size={"s"}>
+                                <Open xml={open} width={16} height={16} />
+                            </Spacer>
+                        )}
+                        <Icon source={{ uri: icon }} />
+                    </Status>
+                </Icons>
+            </RestaurantInfo>
+        </RestaurantCard>
+    );
 });
 
 export default React.memo(RestaurantInfoCard);
