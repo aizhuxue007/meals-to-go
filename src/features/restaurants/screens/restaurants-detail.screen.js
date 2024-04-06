f316118ae7c3adbbde9521fb953312452010989c
Aizhu Xue <aizhudeveloper@gmail.com>
2024-04-06 08:31:26 -0400

    add list accordion to restaurantdetail screen

--- src/features/restaurants/screens/restaurants-detail.screen.js
+++ src/features/restaurants/screens/restaurants-detail.screen.js
@@ -1,12 +1,41 @@
-import { SafeArea } from "../../../components/utility/safe-area.component";
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
+import { List } from "react-native-paper";
 
 const RestaurantDetailScreen = ({ route }) => {
     const { restaurant } = route.params
     return (
-        <SafeArea>
+        <>
             <RestaurantInfoCard restaurant={restaurant} />
-        </SafeArea>
+
+            <List.Accordion
+                title="Breakfast"
+                left={props => <List.Icon {...props} icon="bread-slice"
+                />}>
+                <List.Item title="Eggs Benedict" />
+                <List.Item title="Classic Breakfast" />
+            </List.Accordion>
+            <List.Accordion
+                title="Lunch"
+                left={props => <List.Icon {...props} icon="hamburger" />}>
+                <List.Item title="Eggs Benedict" />
+                <List.Item title="Classic Breakfast" />
+            </List.Accordion>
+            <List.Accordion
+                title="Dinner"
+                left={props => <List.Icon {...props} icon="silverware-fork-knife" />}>
+                <List.Item title="Eggs Benedict" />
+                <List.Item title="Classic Breakfast" />
+            </List.Accordion>
+            <List.Accordion
+                title="Drinks"
+                left={props => <List.Icon {...props} icon="bottle-soda" />}>
+                <List.Item title="Eggs Benedict" />
+                <List.Item title="Classic Breakfast" />
+            </List.Accordion>
+
+
+
+        </>
     )
 }
 
