4212243c9309813fb8d77305896d6e85798e62d8
Aizhu Xue <aizhudeveloper@gmail.com>
2024-04-06 07:41:10 -0400

    add Safe area to restaurantdetail screen

--- src/features/restaurants/screens/restaurants-detail.screen.js
+++ src/features/restaurants/screens/restaurants-detail.screen.js
@@ -1,8 +1,13 @@
+import { SafeArea } from "../../../components/utility/safe-area.component";
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 
 const RestaurantDetailScreen = ({ route }) => {
     const { restaurant } = route.params
-    return (<RestaurantInfoCard restaurant={restaurant} />)
+    return (
+        <SafeArea>
+            <RestaurantInfoCard restaurant={restaurant} />
+        </SafeArea>
+    )
 }
 
 export default RestaurantDetailScreen
