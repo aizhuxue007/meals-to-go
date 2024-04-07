d68be6b59664c578e055b8f27ca81b248026b802
Aizhu Xue <aizhudeveloper@gmail.com>
2024-04-07 18:07:21 -0400

    add markers to map

--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -1,14 +1,17 @@
 import React, { useState, useEffect, useContext } from "react"
-import MapView from 'react-native-maps'
+import MapView, { Marker } from 'react-native-maps'
 import { SafeArea } from '../../../components/utility/safe-area.component'
 import Search from '../components/search.component'
 import { LocationContext } from '../../location/location.context'
-import restaurantsContext from '../../../services/restaurants/restaurants.context'
+import { RestaurantsContext } from '../../../services/restaurants/restaurants.context'
 
 export const MapScreen = () => {
     const [latDelta, setLatDelta] = useState(null)
     const { location } = useContext(LocationContext)
+
     const { lat, lng, viewport } = location
+    const { restaurants } = useContext(RestaurantsContext)
+
     useEffect(() => {
         const neLat = viewport.northeast.lat
         const swLat = viewport.southwest.lat
@@ -26,7 +29,21 @@ export const MapScreen = () => {
                     latitudeDelta: latDelta,
                     longitudeDelta: 0.02
                 }}
-            />
+            >
+                {restaurants && restaurants.map(restaurant => {
+                    console.log("from map screen", restaurant.geometry.location)
+                    return (
+                        <Marker
+                            key={restaurant.name}
+                            title={restaurant.name}
+                            coordinate={{
+                                latitude: restaurant.geometry.location.lat,
+                                longitude: restaurant.geometry.location.lng
+                            }}
+                        />
+                    )
+                })}
+            </MapView>
         </SafeArea>
     )
 }
--- src/features/restaurants/components/restaurant-list.component.js
+++ src/features/restaurants/components/restaurant-list.component.js
@@ -4,20 +4,19 @@ import { styled } from "styled-components/native";
 styled;
 
 const RestaurantFlatList = styled(FlatList).attrs({
-  contentContainerStyle: { padding: 16 },
+    contentContainerStyle: { padding: 16 },
 })``;
 
 const RestaurantList = ({ restaurants, renderItem1 }) => {
-  console.log("from restaurant list", restaurants);
-  return (
-    <RestaurantFlatList
-      data={restaurants}
-      renderItem={renderItem1}
-      keyExtractor={(e, i) => i}
-      bounces={false}
-      overScrollMode="never"
-    />
-  );
+    return (
+        <RestaurantFlatList
+            data={restaurants}
+            renderItem={renderItem1}
+            keyExtractor={(e, i) => i}
+            bounces={false}
+            overScrollMode="never"
+        />
+    );
 };
 
 export default RestaurantList;
