aaec5c8be2ceb65154188f98a5a3be7234122c6d
Aizhu Xue <aizhudeveloper@gmail.com>
2024-04-07 09:25:08 -0400

    set up map region

--- src/features/location/location.service.js
+++ src/features/location/location.service.js
@@ -2,22 +2,23 @@ import camelize from "camelize";
 import { locations } from "./location.mock";
 
 export const locationRequest = (searchTerm) => {
-  if (!searchTerm.length) {
-    console.log("from locationRequest, empty string");
-    return;
-  }
-  return new Promise((resolve, reject) => {
-    const mockLocation = locations[searchTerm];
-    if (!mockLocation) {
-      reject("mockLocation not found!");
+    if (!searchTerm.length) {
+        console.log("from locationRequest, empty string");
+        return;
     }
-    resolve(mockLocation);
-  });
+    return new Promise((resolve, reject) => {
+        const mockLocation = locations[searchTerm];
+        if (!mockLocation) {
+            reject("mockLocation not found!");
+        }
+        resolve(mockLocation);
+    });
 };
 
 export const locationTransform = (location) => {
-  const formattedResult = camelize(location.results[0]);
-  const { geometry } = formattedResult;
-  const { lat, lng } = geometry.location;
-  return { lat, lng };
+    const formattedResult = camelize(location.results[0]);
+    const { geometry } = formattedResult;
+    const { lat, lng } = geometry.location;
+    console.log(geometry)
+    return { lat, lng, viewport: geometry.viewport };
 };
--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -1,12 +1,32 @@
+import React, { useState, useEffect, useContext } from "react"
 import MapView from 'react-native-maps'
 import { SafeArea } from '../../../components/utility/safe-area.component'
 import Search from '../components/search.component'
+import { LocationContext } from '../../location/location.context'
+import restaurantsContext from '../../../services/restaurants/restaurants.context'
 
 export const MapScreen = () => {
+    const [latDelta, setLatDelta] = useState(null)
+    const { location } = useContext(LocationContext)
+    const { lat, lng, viewport } = location
+    useEffect(() => {
+        const neLat = viewport.northeast.lat
+        const swLat = viewport.southwest.lat
+        setLatDelta(neLat - swLat)
+    }, [location, viewport])
+
     return (
         <SafeArea style={{ position: 'relative' }}>
             <Search />
-            <MapView style={{ height: '100%' }} />
+            <MapView
+                style={{ height: '100%' }}
+                region={{
+                    latitude: lat,
+                    longitude: lng,
+                    latitudeDelta: latDelta,
+                    longitudeDelta: 0.02
+                }}
+            />
         </SafeArea>
     )
 }
