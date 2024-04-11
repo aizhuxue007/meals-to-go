a06477f704d3df1c3eca6e5c076cc42efeeb498e
Aizhu Xue <aizhudeveloper@gmail.com>
2024-04-11 19:28:56 -0400

    add isAuthed to context 2

--- src/services/authentification/authentification.context.js
+++ src/services/authentification/authentification.context.js
@@ -46,6 +46,7 @@ export const AuthContextProvider = ({ children }) => {
                 isLoading,
                 error,
                 onLogin,
+                isAuthenticated,
             }}
         >
             {children}
