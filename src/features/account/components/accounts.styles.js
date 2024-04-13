bdecf2a7a745795593b2532b51bf647262ede708
Aizhu Xue <aizhudeveloper@gmail.com>
2024-04-12 20:26:55 -0400

    add white overlay to accounts screen

--- src/features/account/components/accounts.styles.js
+++ src/features/account/components/accounts.styles.js
@@ -9,3 +9,10 @@ export const TestView = styled(View)`
     justify-content: center;
     align-items: center;
 `
+
+export const AccountsCover = styled(View)`
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(255, 255, 255, 0.3)
+`
--- src/features/account/screens/account.screen.js
+++ src/features/account/screens/account.screen.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Text } from 'react-native'
-import { ImgBg, TestView } from '../components/accounts.styles'
+import { ImgBg, TestView, AccountsCover } from '../components/accounts.styles'
 
 
 const AccountScreen = () => {
     return (
         <ImgBg source={{ uri: "/Users/aizhuxue/dev/react-native/ztm/meals-to-go/assets/home_bg.jpg" }} resizeMode="cover">
+            <AccountsCover />
             <TestView>
                 <Text>Accounts</Text>
             </TestView>
