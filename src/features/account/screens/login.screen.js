51cbb5ca71654055250de003a8d84a67e91c1053
Aizhu Xue <aizhudeveloper@gmail.com>
2024-04-16 07:27:41 -0400

    add activity indicator to login

--- src/features/account/screens/login.screen.js
+++ src/features/account/screens/login.screen.js
@@ -1,4 +1,5 @@
 import React, { useState, useContext } from "react";
+import { ActivityIndicator, MD2Colors } from "react-native-paper";
 import {
     AccountBackground,
     AccountCover,
@@ -13,48 +14,52 @@ import { Text, View } from "react-native";
 const LoginScreen = ({ navigation }) => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const { onLogin, error } = useContext(AuthContext);
+    const { onLogin, error, isLoading } = useContext(AuthContext);
 
     return (
         <AccountBackground>
             <AccountCover />
             <AccountContainer>
                 <Heading>Login</Heading>
-                <AuthInput
-                    label="E-mail"
-                    value={email}
-                    textContentType="emailAddress"
-                    keyboardType="email-address"
-                    autoCapitalize="none"
-                    onChangeText={(u) => setEmail(u)}
-                />
-                <AuthInput
-                    label="Password"
-                    value={password}
-                    textContentType="password"
-                    secureTextEntry
-                    autoCapitalize="none"
-                    onChangeText={(p) => setPassword(p)}
-                />
-                {error.message && (
+                {isLoading ? <ActivityIndicator animating={true} color={MD2Colors.red800} /> : (
                     <View>
-                        <Text>{error.message}</Text>
-                    </View>
-                )}
-                <AuthButton
-                    icon="lock-open-outline"
-                    mode="contained"
-                    onPress={() => onLogin(email, password)}
-                >
-                    Login
-                </AuthButton>
-                <AuthButton
-                    icon="lock-open-outline"
-                    mode="contained"
-                    onPress={() => navigation.goBack()}
-                >
-                    Go Back
-                </AuthButton>
+                        <AuthInput
+                            label="E-mail"
+                            value={email}
+                            textContentType="emailAddress"
+                            keyboardType="email-address"
+                            autoCapitalize="none"
+                            onChangeText={(u) => setEmail(u)}
+                        />
+                        <AuthInput
+                            label="Password"
+                            value={password}
+                            textContentType="password"
+                            secureTextEntry
+                            autoCapitalize="none"
+                            onChangeText={(p) => setPassword(p)}
+                        />
+                        {error.message && (
+                            <View>
+                                <Text>{error.message}</Text>
+                            </View>
+                        )}
+                        <AuthButton
+                            icon="lock-open-outline"
+                            mode="contained"
+                            onPress={() => onLogin(email, password)}
+                        >
+                            Login
+                        </AuthButton>
+                        <AuthButton
+                            icon="lock-open-outline"
+                            mode="contained"
+                            onPress={() => navigation.goBack()}
+                        >
+                            Go Back
+                        </AuthButton>
+                    </View>)}
+
             </AccountContainer>
         </AccountBackground>
     );
