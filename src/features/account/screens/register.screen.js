5b065120747d4ceeb3958b8160568a7ba9ec81c4
Aizhu Xue <aizhudeveloper@gmail.com>
2024-04-16 05:14:22 -0400

    design register from login template

--- src/features/account/screens/register.screen.js
+++ src/features/account/screens/register.screen.js
@@ -1,16 +1,72 @@
-import React from 'react'
-import { Text } from 'react-native'
-import { ImgBg, TestView } from '../components/accounts.styles'
+import React, { useState, useContext } from "react";
+import {
+    AccountBackground,
+    AccountCover,
+    AccountContainer,
+    AuthButton,
+    AuthInput,
+    Heading
+} from "../components/accounts.styles"
+import { AuthContext } from "../../../services/authentification/authentification.context";
+import { Text, View } from "react-native";
 
+const RegisterScreen = ({ navigation }) => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [repeatedPassword, setRepeatedPassword] = useState("");
+    const { onRegister, error } = useContext(AuthContext);
 
-const RegisterScreen = () => {
     return (
-        <ImgBg source={{ uri: "/Users/aizhuxue/dev/react-native/ztm/meals-to-go/assets/home_bg.jpg" }} resizeMode="cover">
-            <TestView>
-                <Text>Register</Text>
-            </TestView>
-        </ImgBg>
-    )
-}
+        <AccountBackground>
+            <AccountCover />
+            <AccountContainer>
+                <Heading>Register</Heading>
+                <AuthInput
+                    label="E-mail"
+                    value={email}
+                    textContentType="emailAddress"
+                    keyboardType="email-address"
+                    autoCapitalize="none"
+                    onChangeText={(u) => setEmail(u)}
+                />
+                <AuthInput
+                    label="Password"
+                    value={password}
+                    textContentType="password"
+                    secureTextEntry
+                    autoCapitalize="none"
+                    onChangeText={(p) => setPassword(p)}
+                />
+                <AuthInput
+                    label="RepeatPassword"
+                    value={repeatedPassword}
+                    textContentType="password"
+                    secureTextEntry
+                    autoCapitalize="none"
+                    onChangeText={(p) => setRepeatedPassword(p)}
+                />
+                {error.message && (
+                    <View>
+                        <Text>{error.message}</Text>
+                    </View>
+                )}
+                <AuthButton
+                    icon="lock-open-outline"
+                    mode="contained"
+                    onPress={() => onRegister(email, password)}
+                >
+                    Register
+                </AuthButton>
+                <AuthButton
+                    icon="lock-open-outline"
+                    mode="contained"
+                    onPress={() => navigation.goBack()}
+                >
+                    Go Back
+                </AuthButton>
+            </AccountContainer>
+        </AccountBackground>
+    );
+};
 
 export default RegisterScreen
