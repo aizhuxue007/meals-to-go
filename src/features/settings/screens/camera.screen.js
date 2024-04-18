0d1491aabd11802660f728bd468e3c63a45253c8
Aizhu Xue <aizhudeveloper@gmail.com>
2024-04-18 16:49:48 -0400

    get front camera to show

--- src/features/settings/screens/camera.screen.js
+++ src/features/settings/screens/camera.screen.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import { Text, View } from 'react-native'
-import { Camera, CameraType } from 'expo-camera';
+import { Camera, CameraType, takePicturesAsync } from 'expo-camera';
 
 const CameraScreen = () => {
+    const [permission, requestPermission] = Camera.useCameraPermissions();
+
+    if (!permission) requestPermission()
+    Camera.take
     return (
-        <View>
+        <View style={{ flex: 1 }}>
             <Camera
-                style={{ flex: 1 }}
+                style={{ flex: 0.75 }}
                 type={CameraType.front}
+                onCameraReady={() => null}
             ></Camera>
-        </View>
+        </View >
     )
 }
 
