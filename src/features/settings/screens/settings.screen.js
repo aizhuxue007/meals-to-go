4d47d7a8979b8a7f9343676e43d0d30e07c063cf
Aizhu Xue <aizhudeveloper@gmail.com>
2024-04-18 11:22:17 -0400

    polish settings

--- src/features/settings/screens/settings.screen.js
+++ src/features/settings/screens/settings.screen.js
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import { Text } from "react-native";
-import { List } from "react-native-paper";
+import { List, Avatar } from "react-native-paper";
 import { styled } from "styled-components/native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { AuthContext } from "../../../services/authentification/authentification.context";
@@ -9,19 +9,31 @@ const SafeArea = styled(SafeAreaView)`
   flex: 1;
 `;
 
+const HeaderContainer = styled.View`
+  align-items: center;
+  gap: ${props => props.theme.space[4]};
+`
+
+const SettingItem = styled(List.Item)`
+  padding: ${props => props.theme.space[3]};
+`
+
 const SettingScreen = ({ navigation }) => {
   const { onLogout } = useContext(AuthContext)
   return (
     <SafeArea>
-      <Text>Settings</Text>
+      <HeaderContainer>
+        <Avatar.Icon size={150} icon="human" />
+        <Text>Settings</Text>
+      </HeaderContainer>
       <List.Section>
-        <List.Item
+        <SettingItem
           title="Favourites"
           description="Liked restaurants"
           left={props => <List.Icon {...props} icon="heart" />}
           onPress={() => navigation.navigate('Favorites')}
         />
-        <List.Item
+        <SettingItem
           title="Logout"
           description=""
           left={props => <List.Icon {...props} icon="door" />}
