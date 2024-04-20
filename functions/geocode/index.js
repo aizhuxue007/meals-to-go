cc1e474c8dc68e56ff14e028f36accaa65dd45f9
Aizhu Xue <aizhudeveloper@gmail.com>
2024-04-20 08:35:56 -0400

    get geocode location to respond with json

--- functions/geocode/index.js
+++ functions/geocode/index.js
@@ -1,3 +1,10 @@
+const { locations: locationsMock } = require("./geocode.mock");
+const url = require('url');
+
 module.exports.geocodeRequest = (request, response) => {
-    response.send("Geocode request")
+    const query = url.parse(request.url, true).query;
+    const { city } = query
+    const location = locationsMock[city.toLowerCase()]
+    console.log(city, location)
+    response.json(location);
 }
--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,6 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
 const { onRequest } = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
+const { geocodeRequest } = require("./geocode")
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
