ecd05b1d72b19acc2e900ee9496d66da9afe906b
Aizhu Xue <aizhudeveloper@gmail.com>
2024-04-21 05:50:55 -0400

    add fetch to firebase locations

--- functions/geocode/index.js
+++ functions/geocode/index.js
@@ -4,6 +4,7 @@ const url = require('url');
 module.exports.geocodeRequest = (request, response) => {
     const query = url.parse(request.url, true).query;
     const { city } = query
+    console.log(city)
     const location = locationsMock[city.toLowerCase()]
     response.json(location);
 }
--- src/services/location/location.service.js
+++ src/services/location/location.service.js
@@ -1,21 +1,16 @@
 import camelize from "camelize";
-import { locations } from "./location.mock";
 
 export const locationRequest = (searchTerm) => {
-    if (!searchTerm.length) {
-        console.log("from locationRequest, empty string");
-        return;
-    }
-    return new Promise((resolve, reject) => {
-        const mockLocation = locations[searchTerm];
-        if (!mockLocation) {
-            reject("mockLocation not found!");
-        }
-        resolve(mockLocation);
-    });
+    console.log(searchTerm)
+    return fetch(`http://127.0.0.1:5001/meals-to-go-38736/us-central1/geocode?city=antwerp`)
+        .then(res => {
+            return res.json()
+        })
+        .catch(err => console.log('from locationreq', err))
 };
 
 export const locationTransform = (location) => {
+    console.log(location)
     const formattedResult = camelize(location.results[0]);
     const { geometry } = formattedResult;
     const { lat, lng } = geometry.location;
