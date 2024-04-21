717669e87661971627bbf687f30fb1193bd6b8d3
Aizhu Xue <aizhudeveloper@gmail.com>
2024-04-21 09:46:13 -0400

    add searchTerm to query

--- src/services/location/location.service.js
+++ src/services/location/location.service.js
@@ -2,9 +2,16 @@ import camelize from "camelize";
 import axios from 'axios'
 
 export const locationRequest = async (searchTerm) => {
-    return await axios.get(`https://3a74-2601-18d-4a7f-d8c0-b08d-77ca-7533-b36.ngrok-free.app/meals-to-go-38736/us-central1/geocode?city=antwerp`)
-        .then(res => { console.log(res.data) })
-        .catch(err => console.log('from locationreq catch method', err))
+    try {
+        const resp = await axios.get(`https://3a74-2601-18d-4a7f-d8c0-b08d-77ca-7533-b36.ngrok-free.app/meals-to-go-38736/us-central1/geocode?city=${searchTerm}`)
+            .then(res => res.data)
+            .catch(err => console.log('from locationreq catch method', err))
+        console.log(resp.result)
+        return resp.result
+    } catch (error) {
+        console.log('from locationRequest', error)
+    }
+    return
 };
 
 export const locationTransform = (location) => {
