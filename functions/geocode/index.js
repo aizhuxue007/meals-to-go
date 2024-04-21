f7e3697aeb4497b54a8fae80d2f2f11922981d7a
Aizhu Xue <aizhudeveloper@gmail.com>
2024-04-21 06:05:39 -0400

    deduce locationReq not hitting geoRequest

--- functions/geocode/index.js
+++ functions/geocode/index.js
@@ -4,7 +4,7 @@ const url = require('url');
 module.exports.geocodeRequest = (request, response) => {
     const query = url.parse(request.url, true).query;
     const { city } = query
-    console.log(city)
+    console.log('in geoRequest', query)
     const location = locationsMock[city.toLowerCase()]
     response.json(location);
 }
--- src/services/location/location.service.js
+++ src/services/location/location.service.js
@@ -1,16 +1,14 @@
 import camelize from "camelize";
 
 export const locationRequest = (searchTerm) => {
-    console.log(searchTerm)
     return fetch(`http://127.0.0.1:5001/meals-to-go-38736/us-central1/geocode?city=antwerp`)
         .then(res => {
             return res.json()
         })
-        .catch(err => console.log('from locationreq', err))
+        .catch(err => console.log('from locationreq catch method', err))
 };
 
 export const locationTransform = (location) => {
-    console.log(location)
     const formattedResult = camelize(location.results[0]);
     const { geometry } = formattedResult;
     const { lat, lng } = geometry.location;
