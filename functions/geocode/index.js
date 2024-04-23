9ce5f64fabdcfa62190db8e7d70b7c916f3ec29f
Aizhu Xue <aizhudeveloper@gmail.com>
2024-04-22 21:26:28 -0400

    migrate restaurants mocks to firebase, cjs

--- functions/geocode/index.js
+++ functions/geocode/index.js
@@ -4,7 +4,6 @@ const url = require('url');
 module.exports.geocodeRequest = (request, response) => {
     const query = url.parse(request.url, true).query;
     const { city } = query
-    console.log('in geoRequest', query)
     const location = locationsMock[city.toLowerCase()]
     response.json(location);
 }
---
+++ functions/places/index.js
@@ -0,0 +1,5 @@
+
+module.exports.placesRequest = (req, res) => {
+    const query = url.parse(request.url, true).query;
+
+}
