78d3e613cbba6c60d4dbb44d492efdd8cb0fc148
Aizhu Xue <aizhudeveloper@gmail.com>
2024-04-24 07:08:49 -0400

    rm firebase env and console log live geocode

--- src/components/utility/env.js
+++ src/components/utility/env.js
@@ -1,4 +1,4 @@
-export const localHost = 'https://5b2d-2601-18d-4a7f-d8c0-b08d-77ca-7533-b36.ngrok-free.app'
+export const localHost = 'https://5b2d-2601-18d-4a7f-d8c0-b08d-77ca-7533-b36.ngrok-free.app/meals-to-go-38736'
 export const liveHost = 'https://geocode-lskqsnyqga-uc.a.run.app/meals-to-go-38736'
 
 export const isDevelopment = process.env.NODE_ENV === 'development'
--- src/services/location/location.service.js
+++ src/services/location/location.service.js
@@ -4,8 +4,9 @@ import { host } from "../../components/utility/env";
 
 export const locationRequest = async (searchTerm) => {
     try {
-        const resp = await axios.get(`${host}/meals-to-go-38736/us-central1/geocode?city=${searchTerm}`)
+        const resp = await axios.get(`${host}/us-central1/geocode?city=${searchTerm}&mock=true`)
             .then(res => res.data.results)
+        console.log(resp)
         return resp
     } catch (error) {
         console.log('from locationRequest', error)
@@ -14,6 +15,7 @@ export const locationRequest = async (searchTerm) => {
 
 export const locationTransform = (location) => {
     const formattedResult = camelize(location[0]);
+    console.log(formattedResult)
     const { geometry } = formattedResult;
     const { lat, lng } = geometry.location;
     return { lat, lng, viewport: geometry.viewport };
