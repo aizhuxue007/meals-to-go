378a6d33d8811f8ceff25a4b5a8deccc1e9ae834
Aizhu Xue <aizhudeveloper@gmail.com>
2024-04-24 06:36:44 -0400

    config env variables with firebase parameterized

--- functions/geocode/index.js
+++ functions/geocode/index.js
@@ -1,11 +1,27 @@
-const { locations: locationsMock } = require("./geocode.mock");
 const url = require('url');
+const { onRequest } = require('firebase-functions/v2/https');
+const { defineString } = require('firebase-functions/params');
+const { locations: locationsMock } = require("./geocode.mock");
 
-module.exports.geocodeRequest = (request, response, client) => {
+const googleApiKey = defineString('GOOGLE_API_KEY')
+
+module.exports.geocodeRequest = onRequest({}, (request, response, client) => {
     const { city, mock } = url.parse(request.url, true).query;
     if (mock === 'true') {
         const location = locationsMock[city.toLowerCase()]
         return response.json(location);
     }
-    response.send('something else')
-}
+
+    client.geocode({
+        params: {
+            address: city,
+            key: googleApiKey.value()
+        },
+        timeout: 1000,
+    })
+        .then(res => response.json(res.data))
+        .catch(err => {
+            response.status(400)
+            return response.send(err.response.data.error_message)
+        })
+})
