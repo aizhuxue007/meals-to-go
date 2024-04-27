5bdd13c350a943b2f1275702611af8122c317d2e
Aizhu Xue <aizhudeveloper@gmail.com>
2024-04-27 12:56:28 -0400

    fail add spacing to restaurant cards

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -37,53 +37,57 @@ export const RestaurantInfoCard = React.memo(({ restaurant }) => {
 
   const starRatings = [...Array(5)].map((_, i) => i);
   return (
-    <RestaurantCard elevation={5}>
-      <Favourite restaurant={restaurant} />
-      <RestaurantCardCover key={name} source={{ uri: photos && photos[0] }} />
-      <RestaurantInfo>
-        <RestaurantDescription>
-          <Spacer position={"bottom"} size={"s"}>
-            <Text variant={"bold"}>{name}</Text>
-          </Spacer>
-          <Spacer position={"bottom"} size={"m"}>
-            <Text variant={"body"}>{vicinity}</Text>
-          </Spacer>
+    <Spacer position="bottom" size="l">
+      <RestaurantCard elevation={5}>
+        <Favourite restaurant={restaurant} />
+        <RestaurantCardCover key={name} source={{ uri: photos && photos[0] }} />
+        <RestaurantInfo>
+          <RestaurantDescription>
+            <Spacer position={"bottom"} size={"s"}>
+              < Text variant={"bold"} > {name}</Text >
+            </Spacer >
+            <Spacer position={"bottom"} size={"m"}>
+              <Text variant={"body"}>{vicinity}</Text>
+            </Spacer>
 
-          {isClosedTemporarily && (
-            <Text variant={"error"}>Temporarily Closed</Text>
-          )}
-        </RestaurantDescription>
-        <Icons>
-          <Ratings>
-            {starRatings.map((i) =>
-              i < Math.ceil(rating) ? (
-                <Star
-                  key={`${placeId}-${i}-yellow`}
-                  xml={star}
-                  width={20}
-                  height={20}
-                />
-              ) : (
-                <Star
-                  key={`${placeId}-${i}-grey`}
-                  xml={greyStar}
-                  width={20}
-                  height={20}
-                />
-              ),
-            )}
-          </Ratings>
-          <Status>
-            {isOpenNow && (
-              <Spacer position={"top"} size={"s"}>
-                <Open xml={open} width={16} height={16} />
-              </Spacer>
-            )}
-            <Icon source={{ uri: icon }} />
-          </Status>
-        </Icons>
-      </RestaurantInfo>
-    </RestaurantCard>
+            {
+              isClosedTemporarily && (
+                <Text variant={"error"}>Temporarily Closed</Text>
+              )
+            }
+          </RestaurantDescription >
+          <Icons>
+            <Ratings>
+              {starRatings.map((i) =>
+                i < Math.ceil(rating) ? (
+                  <Star
+                    key={`${placeId}-${i}-yellow`}
+                    xml={star}
+                    width={20}
+                    height={20}
+                  />
+                ) : (
+                  <Star
+                    key={`${placeId}-${i}-grey`}
+                    xml={greyStar}
+                    width={20}
+                    height={20}
+                  />
+                ),
+              )}
+            </Ratings>
+            <Status>
+              {isOpenNow && (
+                <Spacer position={"top"} size={"s"}>
+                  <Open xml={open} width={16} height={16} />
+                </Spacer>
+              )}
+              <Icon source={{ uri: icon }} />
+            </Status>
+          </Icons>
+        </RestaurantInfo >
+      </RestaurantCard >
+    </Spacer >
   );
 });
 
