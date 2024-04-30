f652de601c4417795f74129b79c23b4cd0983ad1
Aizhu Xue <aizhudeveloper@gmail.com>
2024-04-30 08:37:55 -0400

    set up stripe client

--- functions/index.js
+++ functions/index.js
@@ -4,6 +4,9 @@ const { placesRequest } = require("./places");
 const { Client } = require("@googlemaps/google-maps-services-js");
 
 const googleClient = new Client({});
+const stripeClient = require("stripe")("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")
+
+console.log(stripeClient)
 
 exports.geocode = onRequest((request, response) => {
   geocodeRequest(request, response, googleClient);
