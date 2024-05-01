695c7b99562494e538208cb469d2e0218480ac79
Aizhu Xue <aizhudeveloper@gmail.com>
2024-04-30 21:11:38 -0400

    set up stripe client

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,23 @@
 const { onRequest } = require("firebase-functions/v2/https");
+const { defineString } = require('firebase-functions/params');
 const { geocodeRequest } = require("./geocode")
+const { payRequest } = require("./pay")
 const { placesRequest } = require("./places")
 const { Client } = require("@googlemaps/google-maps-services-js");
 
-const client = new Client({})
+const STRIPE_SK = defineString('STRIPE_SK');
+
+const stripeClient = require('stripe')(STRIPE_SK);
+const googleClient = new Client({})
+
+exports.pay = onRequest((request, response) => {
+  payRequest(request, response, stripeClient)
+});
 
 exports.geocode = onRequest((request, response) => {
-    geocodeRequest(request, response, client)
+  geocodeRequest(request, response, googleClient)
 });
 
 exports.placesNearby = onRequest((request, response) => {
-    placesRequest(request, response, client)
+  placesRequest(request, response, googleClient)
 })
---
+++ functions/pay/index.js
@@ -0,0 +1,5 @@
+const url = require('url');
+
+module.exports.payRequest = (request, response, client) => {
+    response.send("HElllo from payRequest")
+}
