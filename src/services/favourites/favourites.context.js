03bc59a939c95222de3aacacfebfe2f16a674e55
Aizhu Xue <aizhudeveloper@gmail.com>
2024-05-02 18:39:46 -0400

    copy and paste favourites context solution

--- src/services/favourites/favourites.context.js
+++ src/services/favourites/favourites.context.js
@@ -1,23 +1,27 @@
-import React, { createContext, useState, useEffect } from "react";
+import React, { createContext, useState, useEffect, useContext } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+
+import { AuthenticationContext } from "../../services/authentication/authentication.context";
 
 export const FavouritesContext = createContext();
 
 export const FavouritesContextProvider = ({ children }) => {
+  const { user } = useContext(AuthenticationContext);
+
   const [favourites, setFavourites] = useState([]);
 
-  const saveFavourites = async (value) => {
+  const saveFavourites = async (value, uid) => {
     try {
       const jsonValue = JSON.stringify(value);
-      await AsyncStorage.setItem("@favourites", jsonValue);
+      await AsyncStorage.setItem(`@favourites-${uid}`, jsonValue);
     } catch (e) {
       console.log("error storing", e);
     }
   };
 
-  const loadFavourites = async () => {
+  const loadFavourites = async (uid) => {
     try {
-      const value = await AsyncStorage.getItem("@favourites");
+      const value = await AsyncStorage.getItem(`@favourites-${uid}`);
       if (value !== null) {
         setFavourites(JSON.parse(value));
       }
@@ -25,35 +29,37 @@ export const FavouritesContextProvider = ({ children }) => {
       console.log("error loading", e);
     }
   };
-  const addToFavourites = (restaurant) => {
-    setFavourites((prevFavourites) => {
-      return [...prevFavourites, restaurant];
-    });
-    console.log("in add");
+
+  const add = (restaurant) => {
+    setFavourites([...favourites, restaurant]);
   };
 
-  const removeFromFavourites = (restaurant) => {
-    console.log("in remove");
-    setFavourites((prevFavourites) =>
-      prevFavourites.filter((item) => item.placeId !== restaurant.placeId),
+  const remove = (restaurant) => {
+    const newFavourites = favourites.filter(
+      (x) => x.placeId !== restaurant.placeId
     );
+
+    setFavourites(newFavourites);
   };
 
   useEffect(() => {
-    loadFavourites();
-  }, []);
+    if (user && user.uid) {
+      loadFavourites(user.uid);
+    }
+  }, [user]);
 
   useEffect(() => {
-    saveFavourites(favourites);
-  }, [favourites]);
+    if (user && user.uid && favourites.length) {
+      saveFavourites(favourites, user.uid);
+    }
+  }, [favourites, user]);
 
   return (
     <FavouritesContext.Provider
       value={{
         favourites,
-        setFavourites,
-        addToFavourites,
-        removeFromFavourites,
+        addToFavourites: add,
+        removeFromFavourites: remove,
       }}
     >
       {children}
